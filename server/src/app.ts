6bbf236b1265b5a178afe6cd93eca907876e9c2b
adityanagar-1902 <adnagar19022000@gmail.com>
2026-07-21 15:45:06 +0530

    feat: integrate Better Auth with MongoDB,
    Signed-off-by: adityanagar-1902 <adnagar19022000@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
 import cors from "cors";
 import healthRoute from "./routes/health.route";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
@@ -8,8 +10,9 @@ app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
 }));
-app.use(express.json());
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(express.json());
 app.use("/health", healthRoute);
 
 export default app;
